2b6943939ef1a8f3b03a40800231343a1ba08bf8
Darcio Vilela <darcio@jp7.com.br>
2023-03-17 15:16:30 -0300

    Add more elements

--- apps/api/src/table.ts
+++ apps/api/src/table.ts
@@ -18,6 +18,25 @@ export const table:Table[] = [
         "atomicNumber": 3,
         "groupBlock": "alkali metal",
         "symbol": "Li"
-    }
+    },
+    {
+        "name": "Beryllium",
+        "atomicNumber": 4,
+        "groupBlock": "alkali metal",
+        "symbol": "Be"
+    },
+    {
+        "name": "Boron",
+        "atomicNumber": 5,
+        "groupBlock": "metalloid",
+        "symbol": "B"
+    },
+    {
+        "name": "Carbon",
+        "atomicNumber": 6,
+        "groupBlock": "nonmetal",
+        "symbol": "C"
+    },
+
 
 ]
