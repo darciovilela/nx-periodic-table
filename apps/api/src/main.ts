e1801904e1f63d62113027b2a2f52dd7a4027332
Darcio Vilela <darcio@jp7.com.br>
2023-03-21 16:01:17 -0300

    Modify app to get data from a real API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import * as path from 'path';
 import cors from 'cors';
 import axios from 'axios'
-import { table } from './table'
+// import { table } from './table'
 
 const app = express();
 app.use(cors());
@@ -13,27 +13,40 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api Darcio!' });
 });
 
-// original route to get data from ts file
-app.get('/search', (req, res) => {
-  const q = ((req.query.q as string) ?? '').toLowerCase()
-  res.send(table.filter(({name}) =>
-  name.toLowerCase().includes(q)))
+// Original route to search data from ts local file
+// app.get('/search', (req, res) => {
+//   const q = ((req.query.q as string) ?? '').toLowerCase()
+//   res.send(table.filter(({name}) =>
+//   name.toLowerCase().includes(q)))
+// });
+
+// New route to get periodic table from external API
+app.get('/table', async (req, res) => {
+  try {
+    const response = await axios.get('https://kineticzephyr.onrender.com/periodictable');
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
 });
 
-// Route to use a real API
-// app.get('/search', async (req, res) => {
-//   const q = ((req.query.q as string) ?? '').toLowerCase()
-//   try {
-//     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=1000`);
-//     const data = response.data.results;
-//     const results = data.filter(({ name }) => name.includes(q));
-//     res.send(results);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send('Server Error');
-//   }
+// Route to search data using a real API
+app.get('/search', async (req, res) => {
+  const q = ((req.query.q as string) ?? '').toLowerCase()
+  try {
+    const response = await axios.get('https://kineticzephyr.onrender.com/periodictable');
+    const data = response.data.data;
+    const results = data.filter(({ name }) => name.includes(q));
+    res.send(results);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
 
-// });
+});
+
+
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
